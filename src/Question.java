f3a2ea43b4bb0708e434466446f4539e37a9967e
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-31 20:04:59 -0500

    initial fields, constructor, getters, and abstract methods

--- src/Question.java
+++ src/Question.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Question {
+public abstract class Question {
+
+    private String question;
+    private String questionType;
+    private String[] choices;
+    private int[] answerKey;
+
+
+    public Question(String question, String questionType, String[] choices, int[] answerKey) {
+        this.question = question;
+        this.questionType = questionType;
+        this.choices = choices;
+        this.answerKey = answerKey;
+    }
+
+    // getters
+    public String getQuestion() {
+        return this.question;
+    }
+    public String getQuestionType() {
+        return this.questionType;
+    }
+    public String[] getChoices() {
+        return this.choices;
+    }
+    public int[] getAnswerKey() {
+        return answerKey;
+    }
+
+    // abstract methods
+    public abstract void printQuestionPrompt();
+    public abstract int[] getUserChoice();
+    public abstract boolean checkAnswer();
+
 }
