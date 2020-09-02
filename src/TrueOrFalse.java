8504d81a1309ba114baa4483be6e291692fac74a
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-02 15:34:38 -0500

    implemented askQuestion(), changed answerKey to array of booleans

--- src/TrueOrFalse.java
+++ src/TrueOrFalse.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public abstract class TrueOrFalse extends Question {
 
-    public TrueOrFalse(String question, String questionType, String[] choices, int[] answerKey) {
+    public TrueOrFalse(String question, String questionType, String[] choices, boolean[] answerKey) {
         super(question, questionType, choices, answerKey);
+    }
+
+    @Override
+    public boolean askQuestion() {
+        String[] choices = getChoices();
+        int choiceId;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(getQuestionType() + "\n" + getQuestion());
+        for (int i = 0; i < choices.length; i++) {
+            choiceId = i + 1;
+            System.out.println(choiceId + ") " + choices[i]);
+        }
+        System.out.println("Choose true/false by entering \"1\" or \"2\"");
+        int userChoice = input.nextInt();
+        return getAnswerKey()[userChoice -1];
     }
 }
