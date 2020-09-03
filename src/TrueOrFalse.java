bc924b986153f050048a067420a84b495d3b1ace
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-02 19:26:24 -0500

    minor tweaks

--- src/TrueOrFalse.java
+++ src/TrueOrFalse.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public abstract class TrueOrFalse extends Question {
+public class TrueOrFalse extends Question {
 
     public TrueOrFalse(String question, String questionType, String[] choices, boolean[] answerKey) {
         super(question, questionType, choices, answerKey);
@@ -13,13 +13,13 @@ public abstract class TrueOrFalse extends Question {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println(getQuestionType() + "\n" + getQuestion());
+        System.out.println(getQuestionType() + ": " + getQuestion());
         for (int i = 0; i < choices.length; i++) {
             choiceId = i + 1;
             System.out.println(choiceId + ") " + choices[i]);
         }
         System.out.println("Choose true/false by entering \"1\" or \"2\":");
         int userChoice = input.nextInt();
-        return getAnswerKey()[userChoice -1];
+        return getAnswerKey()[userChoice - 1];
     }
 }
