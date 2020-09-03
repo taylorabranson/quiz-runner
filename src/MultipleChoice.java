229f1f0098ab7492815cf11b67c1ba8f19891936
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-02 19:27:04 -0500

    implemented askQuestion()

--- src/MultipleChoice.java
+++ src/MultipleChoice.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class MultipleChoice extends QuestionSet {
 
     public MultipleChoice(String question, String questionType, String[] choices, boolean[] answerKey) {
         super(question, questionType, choices, answerKey);
+    }
+
+    @Override
+    public boolean askQuestion() {
+        String[] choices = getChoices();
+        int choiceId;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(getQuestionType() + ": " + getQuestion());
+        for (int i = 0; i < choices.length; i++) {
+            choiceId = i + 1;
+            System.out.println(choiceId + ") " + choices[i]);
+        }
+        System.out.println("Choose answer by entering corresponding number:");
+        int userChoice = input.nextInt();
+        return getAnswerKey()[userChoice -1];
     }
 }
